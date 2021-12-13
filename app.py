cbdd0373957941316d0dabd7776087d9fa66989a
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-13 19:56:00 +0530

    added IOError exception block

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#Return True if the query is OK else False
-#-----------------------------------------------#
+# API response: Return True if the query is OK else False
+# -----------------------------------------------#
 
 from flask import *
 import requests
@@ -9,7 +9,6 @@ import re
 import urllib.request
 import speech_recognition as sr
 import torch
-import torchvision
 from PIL import *
 from io import BytesIO
 import torchvision.transforms as transforms
@@ -17,73 +16,125 @@ import numpy as np
 from sightengine.client import SightengineClient
 from googletrans import Translator
 from PhishingWebsite import phishing_detection
-from PhishingWebsite import feature_extraction
 from urlextract import URLExtract
 
-#-----------------------------------------------#
+# ----------------------------------------------- #
 app = Flask(__name__)
 
-hate_model = pickle.load(open(r"hatespeech\saved_models\lr_model.pkl", 'rb'))  #path to hate speech model
-hate_vect = pickle.load(open(r"hatespeech\saved_models\vectorizer.pkl", 'rb')) #path to hate speech vectorizer
+# ---------------------------- Loading Models ---------------------------- #
 
-spam_model = pickle.load(open(r"Spam Classifier\saved_models\lr_model.pkl", 'rb')) #path to spam model
-spam_vect = pickle.load(open(r"Spam Classifier\saved_models\vectorizer.pickle", 'rb')) ##path to spam vectorizer
+# ------------------ Hate Speech ------------------- #
 
-violence_model = pickle.load(open(r"Violence-Detection\Pytorch-Implementation\saved_model\resnetmain.pkl","rb"))
-checkpoint = torch.load(r"Violence-Detection\Pytorch-Implementation\saved_model\main_model.pt",map_location=torch.device('cpu'))
+hate_model = pickle.load(open(r"hatespeech\saved_models\lr_model.pkl", 
+    'rb'))  # path to hate speech model
+hate_vect = pickle.load(open(r"hatespeech\saved_models\vectorizer.pkl", 
+    'rb')) # path to hate speech vectorizer
+
+# ------------------ Spam Detection ------------------- #
+
+spam_model = pickle.load(open(r"Spam Classifier\saved_models\lr_model.pkl", 
+    'rb')) # path to spam model
+spam_vect = pickle.load(open(r"Spam Classifier\saved_models\vectorizer.pickle", 
+    'rb')) # path to spam vectorizer
+
+# ------------------ Voilence Detection ------------------- #
+
+violence_model = pickle.load(open(r"Violence-Detection\Pytorch-Implementation\saved_model\resnetmain.pkl", 
+    "rb"))
+checkpoint = torch.load(r"Violence-Detection\Pytorch-Implementation\saved_model\main_model.pt", 
+    map_location=torch.device('cpu'))
 violence_model.load_state_dict(checkpoint['state_dict'])
+
+#  ---------------------------- Loading Models ---------------------------- #
+
 
 custom_config = r'--oem 3 --psm 6'
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" # Set the tesseract path here
 
-client = SightengineClient('510188098','WaehbLBjT3mYTmnxDsp3')
+client = SightengineClient('510188098','WaehbLBjT3mYTmnxDsp3') # Change Credentials Accordingly
 extractor = URLExtract()
 #-----------------------------------------------#
 
+# To detect spam
 def isspam(string:str) -> bool:
+    """Checks for spam in the given string
+
+    Args:
+      string: A string to be checked
+
+    Returns:
+      Boolean
+
+    """
     string=[string]
     sen_trans = spam_vect.transform(string)
     prediction = spam_model.predict(sen_trans)[0]  #0->ham 1->spam
     return True if prediction else False
 
-
-
+# To detect hate speech
 def ishate(string:str) -> bool:
+    """Checks for hate speech in the given string
+
+    Args:
+      string: A string to be checked
+
+    Returns:
+      Boolean
+
+    """
     string=[string]
     sen_trans = hate_vect.transform(string)
     prediction = hate_model.predict(sen_trans)[0]  #0->normal 1->toxic
     return True if prediction else False
 
+# To detect violence
 def isviolence(img) -> bool:
+    """Checks for violence in the given image
+
+    Args:
+      img: A image object to be checked
+
+    Returns:
+      Boolean
+    
+    Raises:
+      IOError – If the file cannot be found, or the image cannot be opened and identified
+
+    """
     class_names=["safe","unsafe"]
     standard_normalization = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                   std=[0.229, 0.224, 0.225])
-    def load_input_image(img):
-        image = img.convert('RGB')
-        prediction_transform = transforms.Compose([transforms.Resize(size=(224, 224)),
-                                         transforms.ToTensor(),
-                                         standard_normalization])
+    try:
+
+        def load_input_image(img):
+            image = img.convert('RGB')
+            prediction_transform = transforms.Compose([transforms.Resize(size=(224, 224)),
+                                             transforms.ToTensor(),
+                                             standard_normalization])
 
 
-        image = prediction_transform(image)[:3,:,:].unsqueeze(0)
-        return image
+            image = prediction_transform(image)[:3,:,:].unsqueeze(0)
+            return image
 
 
-    def predict_image(model, class_names, img):
-        # load the image and return the predicted breed
-        img = load_input_image(img)
-        model = model.cpu()
-        model.eval()
-        idx = torch.argmax(model(img))
-        return class_names[idx]
+        def predict_image(model, class_names, img):
+            # load the image and return the predicted breed
+            img = load_input_image(img)
+            model = model.cpu()
+            model.eval()
+            idx = torch.argmax(model(img))
+            return class_names[idx]
 
-    def run_app(img):
+        def run_app(img):
 
-        prediction = predict_image(violence_model, class_names, img)
-        return prediction
-    p = run_app(img)
-    return 0 if p=="safe" else 1
+            prediction = predict_image(violence_model, class_names, img)
+            return prediction
+            
+        p = run_app(img)
+        return 0 if p=="safe" else 1
+    except IOError:
+        return 1
 
 #-----------------------------------------------#
 
@@ -95,12 +146,11 @@ def main():
 
     if t == "audio":
         r = sr.Recognizer()
-        path = r"Web-Frontend\static\uploads\audio.wav"
+        path = r"Web-Frontend\static\uploads\audio.wav" # Media storage path. 
         urllib.request.urlretrieve(data["data"], path)
         with sr.AudioFile(path) as source:
             audio=r.record(source)
             extractedInformation = r.recognize_google(audio,show_all=False)
-            # extractedInformation = translator.translate(extractedInformation).text
             if extractedInformation=="":
                 return jsonify(True)
             if re.search("\*",extractedInformation):
@@ -130,7 +180,7 @@ def main():
         if isviolence(img_org):
             return jsonify(False)
 
-        #return jsonify(False) if isviolence(img_org) else jsonify(True)
+        
 
         output = client.check('nudity').set_url(img_url)
         if output['nudity']['safe']<0.5:
@@ -159,8 +209,7 @@ def main():
                 l.append(phishing_detection.getResult(i))
             if "Phishing Website" in l:
                 return jsonify(False)
-        #receivedtext = translator.translate(receivedtext).text
-        # print("Error Idhar hai")
+        
         if ishate(receivedtext):
             return jsonify(False)
         if isspam(receivedtext):
