21007da1751ddf04493c04500b54402b5524ef18
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-15 22:17:41 +0530

    tests: added api response test

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
-try:
-	from app import app
-	import unittest
-except Exception as e:
-	print("{}".format(e))
+
+import app
+import unittest
+
 
 
 class apitest(unittest.TestCase):
 
+	def setUp(self):
+		print("==> Setting up the env for tests!!")
+		app.app.config['Testing']=True
+		self.app = app.app.test_client()
+
 	# Check for response type(200)
 	def test_response(self):
-		tester = app.test_client(self)
-		response = tester.get("")
-		statuscode = response.status_code
-		self.assertEqual(statuscode,200)
+		resp = self.app.get('/')
+		assert "200 OK" == resp.status
 
 if __name__=="__main__":
 	unittest.main()
