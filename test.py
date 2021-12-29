7f0381be01d866d5df27a6a46e73d7a5d7948fcd
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-29 22:45:12 +0530

    refactor: formated test code

--- test.py
+++ test.py
@@ -1,29 +1,27 @@
-
 import app
 import unittest
 
 
-
 class apitest(unittest.TestCase):
+    def setUp(self):
+        print()
+        print("==> Setting up the env for tests!!")
+        app.app.config["Testing"] = True
+        self.app = app.app.test_client()
 
-	def setUp(self):
-		print()
-		print("==> Setting up the env for tests!!")
-		app.app.config['Testing']=True
-		self.app = app.app.test_client()
+    def tearDown(self):
+        print("==> Tearing down after tests!! \n")
 
-	def tearDown(self):
-		print("==> Tearing down after tests!! \n")
-	
-	# Check for response type(200)
-	def test_response(self):
-		resp = self.app.get('/')
-		self.assertEqual(resp.status_code,200)
+    # Check for response type(200)
+    def test_response(self):
+        resp = self.app.get("/")
+        self.assertEqual(resp.status_code, 200)
 
-	# Check for return type(application/json)
-	def test_content(self):
-		resp = self.app.get('/')
-		self.assertEqual(resp.content_type, "application/json")
-if __name__=="__main__":
-	unittest.main()
+    # Check for return type(application/json)
+    def test_content(self):
+        resp = self.app.get("/")
+        self.assertEqual(resp.content_type, "application/json")
 
+
+if __name__ == "__main__":
+    unittest.main()
